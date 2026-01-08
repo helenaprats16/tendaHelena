6f3d0e46734d13605504dd1f9921165af77d42df
helenaprats16 <helenaprats16@gmail.com>
2026-01-08 23:36:43 +0100

    Tema3: Plantilles i distribucio de pagines, imatges..

--- src/Controller/IniciController.php
+++ src/Controller/IniciController.php
@@ -1,15 +1,15 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class IniciController
+class IniciController extends AbstractController
 {
     #[Route('/', name: 'inici', methods: ['GET'])]
     public function inici(): Response
     {
-        return new Response('Projecte Gestió Tenda de Helena Prats');
+        return $this->render(view: 'inici.html.twig');
     }
 }
-?>
--- src/Controller/SeccioController.php
+++ src/Controller/SeccioController.php
@@ -1,36 +1,37 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/seccions', name: 'seccio_')]
-class SeccioController
+class SeccioController extends AbstractController
 {
     private $seccions = array(
         [
-            "codi" => "1",
+            "codi" => 1,
             "nom" => "Roba",
             "descripcio" => "Secció de roba",
             "any" => "2026",
             "articles" => array("Pantalons", "Camisa", "Jersei", "Jaqueta")
         ],
         [
-            "codi" => "2",
+            "codi" => 2,
             "nom" => "Calçat",
             "descripcio" => "Secció de calçat",
             "any" => "2025",
             "articles" => array("Sabatilles", "Botes", "Sandàlies")
         ],
         [
-            "codi" => "3",
+            "codi" => 3,
             "nom" => "Accessoris",
             "descripcio" => "Secció d'accessoris",
             "any" => "2024",
-            "articles" => array("Bossa", "Cinturó", "Barret")
+            "articles" => array("Bolso", "Cinturó", "Sombrero")
         ],
         [
-            "codi" => "4",
+            "codi" => 4,
             "nom" => "Esports",
             "descripcio" => "Secció d'esports",
             "any" => "2023",
@@ -38,11 +39,11 @@ class SeccioController
         ]
     );
 
-
-
+    // Mètode 1: Mostrar detall d'una secció (primera, per evitar conflicte)
     #[Route('/{codi}', name: 'detall', methods: ['GET'], requirements: ['codi' => '\d+'])]
     public function detall(int $codi): Response
     {
+        // Buscar la secció amb el codi especificat
         $resultat = array_filter(
             $this->seccions,
             function ($seccio) use ($codi) {
@@ -50,23 +51,29 @@ class SeccioController
             }
         );
 
+        // Si no s'ha trobat la secció
         if (!$resultat) {
-            return new Response('No s’ha trobat la secció:' . $codi);
-
-        } else {
-            // Torna 1r element
-            $seccio = array_shift($resultat);
-            $articles = implode(', ', $seccio['articles']);
-            $resp = "<h1>{$seccio['nom']}</h1>
-                    <ul>
-                        <li><strong>Descripció:</strong> {$seccio['descripcio']}</li>
-                        <li><strong>Any:</strong> {$seccio['any']}</li>
-                        <li><strong>Articles:</strong> $articles</li>
-                    </ul>";
-
-            return new Response("<html><body>$resp</body></html>");
+            return new Response('No s\'ha trobat la secció amb codi: ' . $codi, 404);
         }
+
+        // Extreu la secció trobada
+        $seccio = array_shift($resultat);
+
+        // Renderitzar la plantilla seccio/detall.html.twig
+        return $this->render('seccio/detall.html.twig', [
+            'seccio' => $seccio
+        ]);
     }
+
+    // Mètode 2: Llistat de totes les seccions
+    #[Route('', name: 'llistat', methods: ['GET'])]
+    public function llistat(): Response
+    {
+        return $this->render('seccio/llistat.html.twig', [
+            'seccions' => $this->seccions
+        ]);
+    }
+
 
 
 }
